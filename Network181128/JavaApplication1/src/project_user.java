694ef7dff3fc778979011e9868cda62429fac37c
Kim bo bae <tree9295@gmail.com>
2018-11-29 16:24:52 +0900

    Network181129

---
+++ Network181128/JavaApplication1/src/project_user.java
@@ -0,0 +1,16 @@
+
+
+
+import java.util.ArrayList;
+
+public class project_user {
+    public String project_name;
+    public String project_date;
+    public String project_role;
+    public project_user(String name, String role, String date)
+    {
+        project_name = name;
+        project_date = date;
+        project_role = role;
+    }
+}
